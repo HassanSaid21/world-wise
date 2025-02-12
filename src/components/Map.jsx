d312d9ac981ac56dd9019309c81c46b20ef014f6
karim said <karimsaid214@gmail.com>
2025-02-12 21:52:35 +0300

    implemented the map component using  react leaflet package  with initial position at [0,40] ,

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,16 +1,26 @@
 import { useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { useState } from "react";
 function Map() {
   const [searchParams , setSearchParams] = useSearchParams();
-
+const  [position , setPosition] = useState([40,0]);
   const lat = searchParams.get("lat");
   const lng = searchParams.get("lng");
+  //const position =[lat , lng]
   return (
     <div className={styles.mapContainer}>
-      <h1>map</h1>
-      <h2>
-        lat:{lat} lng:{lng}
-      </h2>
+       <MapContainer center={position} zoom={13} scrollWheelZoom={true} className={styles.map}>
+    <TileLayer
+      attribution='&copy; <a href="https://www.openstreetmap.fr/hot/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    />
+    <Marker position={position}>
+      <Popup>
+        A pretty CSS3 popup. <br /> Easily customizable.
+      </Popup>
+    </Marker>
+  </MapContainer>
     </div>
   );
 }
